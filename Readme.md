ae3f44d6455af9c47aac8fc03bf0f392633ba29e
JNHFLINT <JNHFLINT@gmail.com>
2026-06-13 01:48:35 -0400

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,6 @@
+6/13/2026 - First day of work on this project
+- Created name "InsightFlow", may get changed but I like this for now.
+- Created an excel spreadsheet with dummy electrician company data to use for basic testing.
+- Ability to read excel spreadsheet so that we can use the data inside.
+- Able to upload file to streamlit.
+- Created boxes for lines of sheets and also visual graphs.
---
+++ data_reader.py
@@ -0,0 +1,45 @@
+import pandas as pd 
+import openpyxl
+import streamlit as st
+import plotly.express as px
+
+# Upload Excel File via streamlit
+uploaded_file = st.file_uploader(
+    "Upload Excel File",
+    type=["xlsx"]
+)
+
+# Read the uploaded Excel file
+if uploaded_file:
+
+    sheets = pd.read_excel(
+        uploaded_file,
+        sheet_name=None
+    )
+
+    # Display the list of sheets found in the Excel file
+    st.write("Sheets Found:")
+    st.write(list(sheets.keys()))
+
+    # Load the data from each sheet
+    jobs_df = sheets["Jobs"]
+    employees_df = sheets["Employees"]
+    monthly_df = sheets["Monthly_Summary"]
+
+    # Create visualizations for data "jobs"
+    fig_jobs = px.line(jobs_df, x="Job_ID", y="Total_Job_Cost")
+    st.subheader("Jobs")
+    st.dataframe(jobs_df)
+    st.plotly_chart(fig_jobs)
+
+    # Create visualizations for data "employees"
+    fig_employees = px.bar(employees_df, x="Employee_ID", y="Total_Worker_Cost")
+    st.subheader("Employees")
+    st.dataframe(employees_df)
+    st.plotly_chart(fig_employees)
+
+    # Create visualizations for data "monthly"
+    fig_monthly = px.line(monthly_df, x="Month", y="Revenue")
+    st.subheader("Monthly Summary")
+    st.dataframe(monthly_df)
+    st.plotly_chart(fig_monthly)
